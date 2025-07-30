3cc41f882dc53ee227e30cf5daa67fcf549f7468
smitkapadiya07 <smitkapadiya07@gmail.com>
2025-07-30 18:22:04 +0530

    changes

---
+++ routes/product.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const {
+    handleSingleGetProducts,
+    handleGetAllProducts,
+    handleUpdateProduct,
+    handleDeleteProduct,
+    handleCreateProduct
+} = require("../controllers/product");
+
+router.get("/", handleGetAllProducts);
+router.put("/:id", handleUpdateProduct);
+router.delete("/:id", handleDeleteProduct);
+router.post("/", handleCreateProduct);
+router.get("/:id", handleSingleGetProducts)
+
+module.exports = router;
